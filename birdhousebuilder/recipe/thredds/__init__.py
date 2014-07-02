0c095191d9962e3a84cdd6772e743fe3547e3129
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-02 20:07:37 +0200

    added config templates

--- birdhousebuilder/recipe/thredds/__init__.py
+++ birdhousebuilder/recipe/thredds/__init__.py
@@ -9,7 +9,9 @@ from mako.template import Template
 import zc.buildout
 from birdhousebuilder.recipe import conda, tomcat
 
-#templ_config = Template(filename=os.path.join(os.path.dirname(__file__), "nginx.conf"))
+wms_config = Template(filename=os.path.join(os.path.dirname(__file__), "wmsConfig.xml"))
+thredds_config = Template(filename=os.path.join(os.path.dirname(__file__), "threddsConfig.xml"))
+catalog_config = Template(filename=os.path.join(os.path.dirname(__file__), "catalog.xml"))
 
 class Recipe(object):
     """This recipe is used by zc.buildout"""
@@ -19,29 +21,68 @@ class Recipe(object):
         b_options = buildout['buildout']
         self.anaconda_home = b_options.get('anaconda-home', conda.anaconda_home)
         self.options['prefix'] = self.anaconda_home
+        self.data_root = options.get('data_root', '/tmp')
 
     def install(self):
         installed = []
-        installed += list(self.install_tomcat())
-        installed += list(self.install_program())
+        installed += list(self.install_thredds())
+        installed += list(self.install_thredds_config())
+        installed += list(self.install_catalog_config())
+        installed += list(self.install_wms_config())
         return installed
 
-    def install_tomcat(self):
+    def install_thredds(self):
         script = conda.Recipe(
             self.buildout,
             self.name,
-            {'pkgs': 'apache-tomcat'})
+            {'pkgs': 'thredds'})
 
         return script.install()
-        
-    def install_program(self):
-        script = supervisor.Recipe(
-            self.buildout,
-            self.name,
-            {'program': 'tomcat',
-             'command': '%s/bin/catalina.sh run' % (self.anaconda_home),
-             })
-        return script.install()
+
+    def install_thredds_config(self):
+        result = thredds_config.render(**self.options)
+
+        output = os.path.join(self.anaconda_home, 'opt', 'apache-tomcat', 'webapps', 'thredds', 'threddsConfig.xml')
+        conda.makedirs(os.path.dirname(output))
+
+        try:
+            os.remove(output)
+        except OSError:
+            pass
+
+        with open(output, 'wt') as fp:
+            fp.write(result)
+        return [output]
+
+    def install_catalog_config(self):
+        result = catalog_config.render(**self.options)
+
+        output = os.path.join(self.anaconda_home, 'opt', 'apache-tomcat', 'webapps', 'thredds', 'catalog.xml')
+        conda.makedirs(os.path.dirname(output))
+
+        try:
+            os.remove(output)
+        except OSError:
+            pass
+
+        with open(output, 'wt') as fp:
+            fp.write(result)
+        return [output]
+
+    def install_wms_config(self):
+        result = wms_config.render(**self.options)
+
+        output = os.path.join(self.anaconda_home, 'opt', 'apache-tomcat', 'webapps', 'thredds', 'wmsConfig.xml')
+        conda.makedirs(os.path.dirname(output))
+
+        try:
+            os.remove(output)
+        except OSError:
+            pass
+
+        with open(output, 'wt') as fp:
+            fp.write(result)
+        return [output]
 
     def update(self):
         return self.install()
