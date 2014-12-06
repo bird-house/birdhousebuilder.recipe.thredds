8a32e498d3081d9826e5d4e5bd9f5325a46ade16
Carsten Ehbrecht <carsten@linacs.org>
2014-12-06 15:30:32 +0100

    release 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of birdhousebuilder.recipe.thredds
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.1.3'
+version = '0.1.4'
 description = 'A Buildout recipe to install and configure thredds server with Anaconda.'
 long_description = (
     open('README.rst').read() + '\n' +
